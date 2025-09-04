b8935e3928f8d320ffccd3c6729f16b95fb823e2
surim250 <miguel.santos03@escola.pr.gov.br>
2025-09-04 10:57:14 -0300

    ok

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const reiniciarBtn = document.getElementById('bth-reiniciar')
 
-avanca.forEach(button =>
+avanca.forEach(button =>{
 button.addEventListener('click', function() {
 const atual= document.querySelector('.ativo');
 const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
 
 
 atual.classList.remove('ativo');
-const proximoElemento= document.getElementById(proximopasso)
+const proximoElemento= document.getElementById(proximoPasso)
 
-
-
-
-
-
-
-})
-
-)
+if (proximoElemento){
+    proximoElemento.classlist.add('ativo');
+} else {
+    console.error(`elemto com ID "${proximoPasso}" não encontrado.`)
+  }
+ });
+});
