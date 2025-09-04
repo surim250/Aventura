0ff66e3ee105821c3c38364f6200fd5c3af8c62e
surim250 <miguel.santos03@escola.pr.gov.br>
2025-09-04 10:36:53 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const reiniciarBtn = document.getElementById('bth-reiniciar')
+
+avanca.forEach(button =>
+button.addEventListener('click', function() {
+const atual= document.querySelector('.ativo');
+const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
+
+
+})
+
+)
